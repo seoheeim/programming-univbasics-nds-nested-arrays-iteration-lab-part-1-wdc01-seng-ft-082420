3829a8cfd241a615ba9cb6ccc29a157d06edb0a1
Seohee  Im <seoheeim@vt.edu>
2020-07-24 18:41:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,6 +8,7 @@ while row_index < src.count do
   element_index = 0 
   while element_index < src[row_index].count do
     if src[row_index][element_index].even?
+    end 
       element_index += 1 
     end
     row_index += 1
